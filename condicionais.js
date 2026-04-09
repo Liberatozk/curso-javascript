36139f7b8713cded342b365c61662d458addc8b0
Liberatozk <rm99738@estudante.fieb.edu.br>
2026-04-09 09:08:21 -0300

    javascript

--- condicionais.js
+++ condicionais.js
@@ -9,4 +9,18 @@ var idade = 16;
 //Condicional simples 
 if(idade > 15){
     console.log("Pode votar!");
+} else {
+    console.log("Não pode votar ainda.");
+}
+
+if(idade > 15 && idade == 16){
+    console.log("Pode votar!")
+} else {
+    console.log("Não pode votar ainda.");
+}
+
+if(idade >= 16) {
+    console.log("Pode votar");
+} else {
+    console.log("Não pode votar ainda.");
 }
---
+++ repeticao.js
@@ -0,0 +1,17 @@
+//enquanto 
+while(false) {
+    console.log("forever!");
+} 
+
+//faca enquanto
+do {
+    console.log("forever!");
+} while(false);
+
+//para faça
+let = i; //contador
+for ( i = 0; i < 5; i++) {
+    console.log("NUmero: " + i);
+}
+//i++ incremento
+
